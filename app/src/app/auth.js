0346164795f855a31b1b97df74f76939f2fef970
Anass Benfillous <anasse.benfillous@gmail.com>
2024-04-07 16:53:42 +0000

    Update auth.js

--- app/src/app/auth.js
+++ app/src/app/auth.js
@@ -13,22 +13,26 @@ const checkAuth = () => {
     window.location.href.includes(r)
   );
 
-  if (!TOKEN && !isPublicPage) {
+  if (!TOKEN && !isPublicPage && !window.location.href.includes("home")) {
     redirectTo("/login");
   } else if (TOKEN && isPublicPage) {
     redirectTo("/welcome");
+  } else if (window.location.href.includes("home")) {
+    document.body.classList.remove("loading-indicator");
   } else if (TOKEN && !isPublicPage) {
     document.body.classList.add("loading-indicator");
-    axios.get("http://127.0.0.1:8000/api/protected-route", {
-      headers: {
-        Authorization: `Bearer ${TOKEN}`,
-      },
-    })
+    axios
+      .get("http://127.0.0.1:8000/api/protected-route", {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+        },
+      })
       .then((response) => {
-        document.body.classList.remove("loading-indicator");
         // Handle successful response if needed
+        document.body.classList.remove("loading-indicator");
       })
       .catch((error) => {
+        document.body.classList.remove("loading-indicator");
         handleAuthError(error);
       });
   }
@@ -44,9 +48,13 @@ const handleAuthError = (error) => {
   if (error.response && error.response.status === 401) {
     localStorage.clear();
     redirectTo("/login");
+  } else if (error.response && error.response.status === 403) {
+    redirectTo("/home");
   } else {
     // Handle other types of errors (network errors, server errors, etc.)
     console.error("An error occurred:", error.message);
+    localStorage.clear();
+    redirectTo("/login");
     // Optionally, display an error message to the user
   }
 };
