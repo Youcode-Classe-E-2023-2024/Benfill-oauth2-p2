ea2c620c5ffeace894afcf31420140bf2054a7f6
Anass Benfillous <anasse.benfillous@gmail.com>
2024-04-07 16:53:51 +0000

    add role column

--- app/src/features/leads/index.js
+++ app/src/features/leads/index.js
@@ -61,7 +61,7 @@ function Leads(){
                         <th>Email Id</th>
                         <th>Created At</th>
                         <th>Status</th>
-                        <th>Assigned To</th>
+                        <th>Role</th>
                         <th></th>
                     </tr>
                     </thead>
@@ -78,15 +78,14 @@ function Leads(){
                                                 </div>
                                             </div>
                                             <div>
-                                                <div className="font-bold">{l.first_name}</div>
-                                                <div className="text-sm opacity-50">{l.last_name}</div>
+                                                <div className="font-bold">{l.name}</div>
                                             </div>
                                         </div>
                                     </td>
                                     <td>{l.email}</td>
                                     <td>{moment(new Date()).add(-5*(k+2), 'days').format("DD MMM YY")}</td>
                                     <td>{getDummyStatus(k)}</td>
-                                    <td>{l.last_name}</td>
+                                    <td>{l.roles[0].role_name}</td>
                                     <td><button className="btn btn-square btn-ghost" onClick={() => deleteCurrentLead(k)}><TrashIcon className="w-5"/></button></td>
                                     </tr>
                                 )
